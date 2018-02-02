388c282fb5f817a4f4e75d2d13cfed929bf11230
Andrei Poenaru <andrei.poenaru@gmail.com>
2018-02-02 13:48:04 +0000

    Retrieve new submissions from watched subreddits.

--- bot.py
+++ bot.py
@@ -1,23 +1,50 @@
 #!/usr/bin/env python3
 
-import sys
+import os.path
+import re
 import requests
+import sys
+import time
 from configparser import ConfigParser
+from datetime import datetime
 
 from praw import Reddit
 
+CONFIG_FILE = 'config.ini'
+LOG_FILE    = 'completed.log'
+
+GFYCAT_API_TOKEN = 'https://api.gfycat.com/v1/oauth/token'
+GFYCAT_API_SEND = 'https://api.gfycat.com/v1/gfycats'
+
 def read_config():
     config = ConfigParser()
-    config.read('config.ini')
+    config.read(CONFIG_FILE)
 
     return config
 
 # Requests gfycat to create a gif from a given URL
 def send_url_to_gfycat(url, token):
-    r = requests.post('https://api.gfycat.com/v1/gfycats',
+    r = requests.post(GFYCAT_API_SEND,
         json={'fetchUrl': url},
         headers={'Authorization': 'Bearer ' + token})
     print('Send:', r.status_code, r.json())
+
+# Mark a submission as already processed, so that it is skipped the next time it's encountered
+def log_processed(submission, gfycat_id):
+    with open(LOG_FILE, 'a') as f:
+        f.write(','.join([str(datetime.now()), submission.id, 'https://reddit.com/r' + submission.permalink, 'https://gfycat.com/' + gfycat_id]) + '\n')
+
+# Get a list of ids of already processed submissions
+def read_processed_submissions():
+    if not os.path.isfile(LOG_FILE):
+        return []
+
+    completed = []
+    with open(LOG_FILE, 'r') as f:
+        for line in f:
+            completed.append(line.split(',')[1])
+
+    return completed
 
 def main():
     config  = read_config()
@@ -33,7 +60,7 @@ def main():
 
     # Get a gfycat token
     gfycat_config = config['gfycat']
-    r = requests.post('https://api.gfycat.com/v1/oauth/token', json={
+    r = requests.post(GFYCAT_API_TOKEN, json={
         'grant_type': 'password',
         'client_id': gfycat_config['client_id'],
         'client_secret': gfycat_config['client_secret'],
@@ -48,6 +75,33 @@ def main():
 
     if (len(sys.argv) > 1):
         send_url_to_gfycat(sys.argv[1], gfycat_token)
+
+    # Read the list of watched subreddits
+    watched_subreddits = re.split('\s+', config['preferences']['subreddits'].strip())
+    print('Watching:', watched_subreddits)
+    watchlist = '+'.join(watched_subreddits)
+
+    # Read the polling interval
+    try:
+        interval = int(config['preferences']['interval'])
+    except(ValueError):
+        print('Invalid interval:', config['preferences']['interval'] + '.', 'Using default: 300.', file=sys.stderr)
+        interval = 300
+
+    # Main loop: get new submissions, find the ones we're interested in, create gfycat mirrors, save them to a file so we don't process them twice, sleep for the specified interval, then repeat
+    while True:
+        already_processed = read_processed_submissions()
+
+        for submission in reddit.subreddit(watchlist).new(limit=10):
+            if(submission.id in already_processed):
+                continue
+
+            # TODO: convert if required
+            print(submission.permalink)
+            log_processed(submission, '???')
+
+        time.sleep(interval)
+        # TODO: we may need to refresh API tokens from time to time
 
 if __name__ == '__main__':
     main()
